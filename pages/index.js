798e91431419b9c6325ca7a611e5e4922fc5580b
isamu <isamumuraguchi.mail@gmail.com>
2025-02-09 12:20:19 +0900

    てすと

--- pages/index.js
+++ pages/index.js
@@ -34,6 +34,7 @@ export default function Home() {
             height={38}
             priority
           />
+          <p>てすと</p>
           <ol>
             <li>
               Get started by editing <code>pages/index.js</code>.
